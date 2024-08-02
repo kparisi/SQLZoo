9f84a75fe21d02687622b7e3a9e60b53801fe920
kparisi <kristenparisi3@gmail.com>
2024-08-02 13:32:27 -0500

    Update 2_SELECTfromworld.sql

--- 2_SELECTfromworld.sql
+++ 2_SELECTfromworld.sql
@@ -93,13 +93,48 @@ For Americas show population in millions and GDP in billions both to 2 decimal p
 SELECT population/
 
 /* 10
+Show the name and per-capita GDP for those countries with a GDP of at least one trillion (1000000000000; that is 12 zeros).
+Round this value to the nearest 1000.
+Show per-capita GDP for the trillion dollar countries to the nearest $1000.
 */
+
+SELECT name, round(gdp/population, -3) FROM world
+WHERE gdp >= 1000000000000
 
 /* 11
+Greece has capital Athens.
+
+Each of the strings 'Greece', and 'Athens' has 6 characters.
+
+Show the name and capital where the name and the capital have the same number of characters.
+
+You can use the LENGTH function to find the number of characters in a string
 */
+
+SELECT name, capital FROM world
+WHERE LENGTH(name)=LENGTH(capital)
 
 /* 12
+The capital of Sweden is Stockholm. Both words start with the letter 'S'.
+
+Show the name and the capital where the first letters of each match. Don't include countries where the name and the capital are the same word.
+You can use the function LEFT to isolate the first character.
+You can use <> as the NOT EQUALS operator
 */
 
+SELECT name, capital FROM world
+WHERE LEFT(name,1) = LEFT(capital,1) AND NOT name = capital
+
 /* 13
+Equatorial Guinea and Dominican Republic have all of the vowels (a e i o u) in the name. They don't count because they have more than one word in the name.
+
+Find the country that has all the vowels and no spaces in its name.
+
+You can use the phrase name NOT LIKE '%a%' to exclude characters from your results.
+The query shown misses countries like Bahamas and Belarus because they contain at least one 'a'
 */
+
+SELECT name
+   FROM world
+WHERE name LIKE '%a%' AND name LIKE '%e%' AND name LIKE '%i%' AND name LIKE '%o%' AND name LIKE '%u%'
+  AND name NOT LIKE '% %'
