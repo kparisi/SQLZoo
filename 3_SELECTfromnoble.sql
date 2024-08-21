4c5b11a3f1b4bc9bc5577409401e27a008c76907
kparisi <kristenparisi3@gmail.com>
2024-08-21 11:50:06 -0400

    Update 3_SELECTfromnoble.sql

--- 3_SELECTfromnoble.sql
+++ 3_SELECTfromnoble.sql
@@ -115,7 +115,10 @@ WHERE winner LIKE 'Sir %'
 ORDER BY yr DESC, winner ASC
 
 
-/*14 */
+/*14.
+The expression subject IN ('chemistry','physics') can be used as a value - it will be 0 or 1.
+Show the 1984 winners and subject ordered by subject and winner name; but list chemistry and physics last.
+  */
 SELECT winner, subject
   FROM nobel
  WHERE yr=1984
