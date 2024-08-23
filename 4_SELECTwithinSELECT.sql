f529b42a699e1057f7fc6d4264e8f227c35931f2
kparisi <kristenparisi3@gmail.com>
2024-08-23 09:39:55 -0400

    Update 4_SELECTwithinSELECT.sql

--- 4_SELECTwithinSELECT.sql
+++ 4_SELECTwithinSELECT.sql
@@ -1,5 +1,6 @@
-/* SELECT within SELECT Tutorial
-
+/*
+SELECT within SELECT Tutorial
+Language:	English  • 日本語 • 中文
 This tutorial looks at how we can use SELECT statements within SELECT statements to perform more complex queries.
 
 name	        continent	area	population	gdp
@@ -12,10 +13,23 @@ Angola	        Africa	    1246700	20609294	100990000000
  */
 
 
-/* */
+/* 1.
+List each country name where the population is larger than that of 'Russia'.
+world(name, continent, area, population, gdp)*/
+
+SELECT name FROM world
+  WHERE population >
+     (SELECT population FROM world
+      WHERE name='Russia')
 
 
-/* */
+/*2.
+Show the countries in Europe with a per capita GDP greater than 'United Kingdom'. */
+
+SELECT name FROM world
+WHERE continent ='Europe' AND gdp/population >(
+SELECT gdp/population FROM world WHERE name = 'United Kingdom'
+)
 
 
 /* */
