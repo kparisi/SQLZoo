02148c5265d3f81ad68ba2932c9caab1abc26a9d
kparisi <kristenparisi3@gmail.com>
2024-08-24 07:59:02 -0400

    Update 4_SELECTwithinSELECT.sql

--- 4_SELECTwithinSELECT.sql
+++ 4_SELECTwithinSELECT.sql
@@ -32,7 +32,9 @@ SELECT gdp/population FROM world WHERE name = 'United Kingdom'
 )
 
 
-/* */
+/*3.
+
+List the name and continent of countries in the continents containing either Argentina or Australia. Order by name of the country. */
 
 
 /* */
