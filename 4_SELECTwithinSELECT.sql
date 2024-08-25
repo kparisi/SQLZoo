34d937dfadc362659f9481e8b34b6e97303ac7f7
kparisi <kristenparisi3@gmail.com>
2024-08-25 17:59:26 -0500

    Update 4_SELECTwithinSELECT.sql

--- 4_SELECTwithinSELECT.sql
+++ 4_SELECTwithinSELECT.sql
@@ -37,7 +37,8 @@ SELECT gdp/population FROM world WHERE name = 'United Kingdom'
 List the name and continent of countries in the continents containing either Argentina or Australia. Order by name of the country. */
 
 
-/* */
+/*4.
+Which country has a population that is more than United Kingdom but less than Germany? Show the name and the population. */
 
 
 /* */
