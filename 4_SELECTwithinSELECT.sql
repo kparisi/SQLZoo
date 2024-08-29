6888b3824b855c2c02e628419b8ba8f54366a7ca
kparisi <kristenparisi3@gmail.com>
2024-08-28 19:30:58 -0500

    Update 4_SELECTwithinSELECT.sql

--- 4_SELECTwithinSELECT.sql
+++ 4_SELECTwithinSELECT.sql
@@ -64,6 +64,9 @@ Austria	11%
 ...	...
  */
 
+SELECT name, CONCAT(ROUND(population/(SELECT population FROM world WHERE name='Germany') * 100,0),'%') AS percentage FROM world
+WHERE continent='Europe'
+
 
 /* */
 
