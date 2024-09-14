aac41de239554e6513896f5c78d7b26ec7a46d53
kparisi <kristenparisi3@gmail.com>
2024-09-14 17:27:54 -0500

    Update 2_SELECTfromworld.sql

--- 2_SELECTfromworld.sql
+++ 2_SELECTfromworld.sql
@@ -131,8 +131,7 @@ Equatorial Guinea and Dominican Republic have all of the vowels (a e i o u) in t
 Find the country that has all the vowels and no spaces in its name.
 
 You can use the phrase name NOT LIKE '%a%' to exclude characters from your results.
-The query shown misses countries like Bahamas and Belarus because they contain at least one 'a'
-*/
+The query shown misses countries like Bahamas and Belarus because they contain at least one 'a' */
 
 SELECT name
    FROM world
